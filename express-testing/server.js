ffd32ba59b0c7694b58f4d6dc534efcf6b69a2a6
Scott McCracken <scottmccracken123@hotmail.com>
2017-11-28 21:04:05 +0000

    added healthy assistant styles and base

--- express-testing/server.js
+++ express-testing/server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
@@ -26,6 +28,6 @@ app.use("*",function(req,res){
   res.sendFile(path + "404.html");
 });
 
-app.listen(3000,function(){
+app.listen(3111,function(){
   console.log("Live at Port 3000");
 });
